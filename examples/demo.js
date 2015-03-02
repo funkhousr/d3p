f723aae2abe8040e39a3b7c46559e6808fabf4e7
Thomas Fankhauser <tommy@southdesign.de>
2015-03-02 15:08:54 +0100

    refactored animation api

--- examples/demo.js
+++ examples/demo.js
@@ -1,7 +1,3 @@
-// Animation Presets
-d3p.animate.appear = d3p.transitions.appear({ duration: 500 });
-d3p.animate.popOut = d3p.transitions.popOut({ duration: 300 });
-
 // Slides
 d3p.slides = [
   [
@@ -10,7 +6,7 @@ d3p.slides = [
       objects.title.append("text").text("d3p: D3 Present Framework").attr("class", "h1");
       
       objects.subtitle = d3p.theme.default.group(stage, 0, 0.1);
-      d3p.theme.phd.heading(objects.subtitle, "h2", "Using d3.js and SVG to create rich animated presentations in the browser");
+      d3p.theme.phd.heading(objects.subtitle, "h2", "Using d3.js and SVG to create animated presentations in the browser");
 
       objects.author = d3p.theme.default.group(stage, 0, 0.8);
       [
@@ -21,18 +17,28 @@ d3p.slides = [
         d3p.theme.phd.heading(objects.author, author[0], author[1]).attr("transform", "translate(0," + d3p.y(i * 0.1) + ")");
       });
 
-      animate.sequence("appear", [objects.title, objects.subtitle]);
-      animate.object("popOut", objects.author);
+      animate.sequence("fadeIn", [objects.title, objects.subtitle], { duration: 1000 });
+      animate.object("fadeIn", objects.author);
       next();
+    },
+    function(stage, objects, animate, next){
+      animate.parallel("popOut", objects);
+      next(true);
     }
   ],
   [
     function(stage, objects, animate, next){
       objects.title = stage.append("text").text("Intro").attr("x", "0").attr("y", "0");
-      animate.appear(objects.title, next);
+      animate.object("fadeIn", objects.title);
+      next();
     },
     function(stage, objects, animate, next){
       objects.p2 = stage.append("text").text("And so on").attr("x", "0").attr("y", "10%");
+      animate.object("fadeIn", objects.p2);
+      next();
+    },
+    function(stage, objects, animate, next){
+      animate.parallel("fadeOut", objects);
       next();
     }
   ]
