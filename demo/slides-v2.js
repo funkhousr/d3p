3e2b7498d2c86bad74c799b3691d96cf1869648d
Thomas Fankhauser <tommy@southdesign.de>
2015-03-06 19:04:12 +0100

    proposal for new slide api

---
+++ demo/slides-v2.js
@@ -0,0 +1,62 @@
+d3p.slides.push(function(slide){
+  
+  slide.objects.title = d3p.theme.default.group(stage, 0, -0.1);
+  d3p.theme.phd.title(slide.objects.title, "d3p: D3 Present Framework");
+
+  slide.objects.subtitle = d3p.theme.default.group(stage, 0, 0.1);
+  d3p.theme.phd.heading(slide.objects.subtitle, "h2", "Using d3.js and SVG to create animated presentations in the browser");
+
+  slide.objects.author = d3p.theme.default.group(stage, 0, 0.7);
+  [
+    ["h3", "Thomas Fankhauser"],
+    ["h4", "tommy@southdesign.de"],
+    ["h4", "@tommyfankhauser"]
+  ].forEach(function(author, i){
+    d3p.theme.phd.heading(slide.objects.author, author[0], author[1]).attr("transform", "translate(0," + d3p.y(i * 0.1) + ")");
+  });
+
+  // Fragments
+  slide.fragments.push(function(fragment){
+    fragment.anmiate.object("fadeIn", slide.objects.title);
+    fragment.next();
+  });
+
+  slide.fragments.push(function(fragment){
+    fragment.anmiate.object("fadeIn", slide.objects.title);
+    // next(); - make optional?
+  });
+
+  slide.next(); // make optional ?
+});
+
+// Slide 2
+d3p.slides.push(function(){
+  
+  this.objects.title = d3p.theme.default.group(stage, 0, -0.1);
+  d3p.theme.phd.title(this.objects.title, "d3p: D3 Present Framework");
+
+  this.objects.subtitle = d3p.theme.default.group(stage, 0, 0.1);
+  d3p.theme.phd.heading(this.objects.subtitle, "h2", "Using d3.js and SVG to create animated presentations in the browser");
+
+  // Fragments
+  this.fragments.push(function(){
+    this.anmiate.object("fadeIn", this.objects.title);
+  });
+
+  this.fragments.push(function(){
+    this.anmiate.object("fadeOut", this.objects.title);
+  });
+
+  this.next();
+});
+
+
+// Wait for webfonts, then go!
+document.addEventListener('DOMContentLoaded', function(){
+  WebFont.load({
+    custom: { families: ['Open Sans Condensed:n3,n7,i3'] },
+    active: d3p.init
+  });
+});
+  
+
