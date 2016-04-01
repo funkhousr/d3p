c57aca834a6d798e5e3df1c71189b9abcc3196a2
Thomas Fankhauser <tommy@southdesign.de>
2016-04-01 18:46:48 +0200

    Removed webfonts dependency and much API.

--- d3p.js
+++ d3p.js
@@ -1,5 +1,7 @@
 var d3p = {
-  slides: []
+  slides: [],
+  objects: {},
+  cache: {}
 };
 
 // d3p.init {{{
@@ -10,13 +12,17 @@ d3p.init = function(config){
     d3p.config[key] = config[key];
   }
 
-  // Simplify API
-  d3p.width    = d3p.config.width;
-  d3p.height   = d3p.config.height;
-  d3p.next     = d3p.slide.next;
-  d3p.previous = d3p.slide.previous;
-  d3p.x        = d3p.stage.x;
-  d3p.y        = d3p.stage.y;
+  // API
+  d3p.width       = d3p.config.width;
+  d3p.height      = d3p.config.height;
+  d3p.next        = d3p.slide.next;
+  d3p.previous    = d3p.slide.previous;
+  d3p.x           = d3p.stage.x;
+  d3p.y           = d3p.stage.y;
+  d3p.translate   = d3p.make.translate;
+  d3p.group       = d3p.make.group;
+  d3p.image       = d3p.make.image;
+  d3p.svg         = d3p.make.svg;
 
   // Print Mode
   if(d3p.print.setup()) return;
@@ -58,7 +64,7 @@ d3p.print = {
     d3p.slides.forEach(function(slide, i){
       d3p.stage.setup();
       d3p.slide.setup();
-      slide(d3p.slide.current, d3p.animations.run);
+      slide(d3p.slide.current);
     });
   }
 };
@@ -107,16 +113,12 @@ d3p.stage = {
     d3p.stage.svg      = d3.select("body").append("svg").attr("class", "d3p");
     d3p.stage.position = d3p.stage.svg.append("g");
     d3p.stage.main     = d3p.stage.position.append("g").attr("class", "stage");
-
-    // Add Background
-    d3p.theme.default.background.klass(d3p.stage.main, "stage-background");
-
     d3p.stage.resize();
     if(!d3p.print.on) window.onresize = d3p.stage.resize;
   },
   resize: function(){
-    d3p.stage.targetWidth  = d3p.print.on ? document.body.clientWidth : window.innerWidth;
-    d3p.stage.targetHeight = d3p.print.on ? document.body.clientHeight : window.innerHeight;
+    d3p.stage.targetWidth  = d3p.print.on ? document.body.clientWidth : window.innerWidth - 1;
+    d3p.stage.targetHeight = d3p.print.on ? document.body.clientHeight : window.innerHeight - 1;
     d3p.stage.scaleWidth   = d3p.stage.targetWidth/d3p.width;
     d3p.stage.scaleHeight  = d3p.stage.targetHeight/d3p.height;
     d3p.stage.scale        = d3p.stage.scaleWidth < d3p.stage.scaleHeight ? d3p.stage.scaleWidth : d3p.stage.scaleHeight;
@@ -145,8 +147,6 @@ d3p.slide = {
   setup: function(){
     d3p.slide.current = {
       stage     : d3p.stage.main,
-      animate   : d3p.animations.api,
-      make      : d3p.theme.default,
       objects   : [],
       fragments : []
     };
@@ -156,14 +156,12 @@ d3p.slide = {
 
     d3p.stage.clear();
     d3p.slide.setup();
-    d3p.slides[d3p.slide.index](d3p.slide.current, d3p.animations.run);
-    if(d3p.slides[d3p.slide.index].length <= 1) d3p.animations.run();
+    d3p.slides[d3p.slide.index](d3p.slide.current);
   },
   fragment: {
     show: function(){
       var fragment = d3p.slide.current.fragments.shift();
-      fragment(d3p.slide.current, d3p.animations.run);
-      if(fragment.length <= 1) d3p.animations.run();
+      fragment(d3p.slide.current);
     }
   },
   next: function(){
@@ -184,156 +182,39 @@ d3p.slide = {
     
     d3p.slide.index--;
     d3p.slide.show();
-  },
-};
-// }}}
-// d3p.theme {{{
-d3p.theme = {
-  default: {
-    translate: function(object, x, y){
-      object.attr("transform", "translate(" + d3p.x(x) + "," + d3p.y(y) + ")");
-    },
-    group: function(stage, x, y, klass){
-      var g = stage.append("g");
-      if(x || y) g.attr("transform", "translate(" + d3p.x(x || 0) + "," + d3p.y(y || 0) + ")");
-      if(klass)  g.attr("class", klass);
-      return g;
-    },
-    image: function(stage, src, width, height, klass){
-      var img = stage.append("image").attr("xlink:href", src).attr("width", width).attr("height", height);
-      if(klass)  img.attr("class", klass);
-      return img;
-    },
-    text: function(stage, text, klass, size){
-      var t = stage.append("text").text(text);
-      if(klass) t.attr("class", klass);
-      if(size)  t.attr("font-size", size);
-      return t;
-    },
-    bubble: function(stage, x, y, r, klass){
-      var g = d3p.theme.default.group(stage, x, y),
-          b = g.append("circle").attr("r", r);
-      if(klass) g.attr("class", klass);
-      return g;
-    },
-    svg:{
-      image: function(stage, src, loaded){
-        d3.xml(src, "image/svg+xml", function(xml){
-          var svg = document.importNode(xml.documentElement, true),
-          layers = {},
-          n       = svg.children.length;
-          for(var i = 0; i < n; i++){
-            var l = stage.node().appendChild(svg.children[0]),
-            layer = d3.select(l);
-            layers[layer.attr("id")] = layer;
-          }
-          if(loaded) loaded(layers);
-        });
-      return stage;
-      },
-    polylineToPath: function(stage){
-        stage.selectAll("polyline").each(function(){
-          var path = d3.select(this.parentNode).append("path"),
-              poly = d3.select(this),
-              a    = this.attributes.length;
-
-          path.attr("d", "M" + poly.attr("points"));
-          for(var i = 0; i < a; i++){
-            if(this.attributes[i] === "points") continue;
-            path.attr(this.attributes[i].name, this.attributes[i].value);
-          }
-          poly.remove();
-        });
-    },
-    lineToPath: function(stage){
-        stage.selectAll("line").each(function(){
-          var path = d3.select(this.parentNode).append("path"),
-              line = d3.select(this),
-              a    = this.attributes.length;
-
-          path.attr("d", ["M", line.attr("x1"), ",", line.attr("y1"), " ", line.attr("x2"), ",", line.attr("y2")].join(""));
-          for(var i = 0; i < a; i++){
-            if(["x1", "x2", "y1", "y2"].indexOf(this.attributes[i]) > -1) continue;
-            path.attr(this.attributes[i].name, this.attributes[i].value);
-          }
-          line.remove();
-        });
-      }
-    },
-    background: {
-      color: function(stage, color){
-        return stage.append("rect").attr("x", d3p.x(-1)).attr("y", d3p.y(-1)).attr("width", d3p.width).attr("height", d3p.height).style("fill", color);
-      },
-      klass: function(stage, klass){
-        return stage.append("rect").attr("x", d3p.x(-1)).attr("y", d3p.y(-1)).attr("width", d3p.width).attr("height", d3p.height).attr("class", klass);
-      },
-      image: function(stage, src){
-        return d3p.theme.default.image(stage, src, d3p.width, d3p.height).attr("y", d3p.y(-1)).attr("x", d3p.x(-1));
-      }
-    }
   }
 };
 // }}}
-// d3p.transitions {{{
-d3p.transitions = {
-  fadeIn: {
-    setup: function(object, params){
-      object.style("opacity", 0); 
-    },
-    run:   function(object, params, next){
-      object.transition()
-        .duration(params.duration || 500)
-        .style("opacity", 1)
-        .each("end", next);
-    }
+// d3p.make {{{
+d3p.make = {
+  translate: function(object, x, y){
+    object.attr("transform", "translate(" + d3p.x(x || 0) + "," + d3p.y(y || 0) + ")");
+    return object;
   },
-  fadeOut: {
-    run:   function(object, params, next){
-      object.transition()
-        .duration(params.duration || 500)
-        .style("opacity", 0)
-        .each("end", next);
-    }
+  group: function(x, y){
+    var g = d3p.stage.main.append("g");
+    d3p.translate(g, x, y);
+    return g;
   },
-  popIn: {
-    setup: function(object, params){
-      object.style("opacity", 0).attr("transform", "scale(" + (params.scale || 4) + ")");
-    },
-    run: function(object, params, next){
-      object.transition()
-        .duration(params.duration || 500)
-        .attr("transform", "scale(1)")
-        .style("opacity", 1)
-        .each("end", next);
-    }
+  image: function(src, width, height){
+    return d3p.stage.main.append("image")
+      .attr("xlink:href", src)
+      .attr("width", width)
+      .attr("height", height);
   },
-  popOut: {
-    run: function(object, params, next){
-      object.transition()
-        .duration(params.duration || 500)
-        .attr("transform", "scale(" + (params.scale || 4) + ")")
-        .style("opacity", 0)
-        .each("end", next);
-    }
-  },
-  drawPaths: {
-    setup: function(object, params){
-      d3p.theme.default.svg.polylineToPath(object);
-      d3p.theme.default.svg.lineToPath(object);
-      object.selectAll("path").each(function(){
-        var l = this.getTotalLength();
-        d3.select(this).attr("stroke-dasharray", l + " " + l).attr("stroke-dashoffset", l);
-      });
-    },
-    run: function(object, params, next){
-      object.selectAll("path").each(function(){
-        var l = this.getTotalLength();
-        d3.select(this).transition()
-        .duration(params.duration || 2000)
-        .attr("stroke-dashoffset", 0)
-        .each("end", next);
-      });
-    }
+  svg: function(src, loaded) {
+    d3.xml(src, "image/svg+xml", function(xml){
+      var svg    = document.importNode(xml.documentElement, true),
+          layers = {},
+          n      = svg.children.length;
+
+      for(var i = 0; i < n; i++){
+        var l     = d3p.stage.main.node().appendChild(svg.children[0]),
+            layer = d3.select(l);
+        layers[layer.attr("id")] = layer;
+      }
+      if(loaded) loaded(layers);
+    });
   }
 };
 // }}}
--- example/slides.js
+++ example/slides.js
@@ -1,40 +1,26 @@
 d3p.slides.push(function(slide){
-  slide.objects.title = slide.make.group(slide.stage, 0, -0.1);
-  d3p.theme.phd.title(slide.objects.title, "d3p: Presentation Template");
+  slide.objects.title = d3p.group(0, 0)
+    .append("text")
+    .text("d3p: Hello World");
 
-  slide.objects.subtitle = slide.make.group(slide.stage, 0, 0.1);
-  d3p.theme.phd.heading(slide.objects.subtitle, "h2", "Start your presentation from here");
-
-  slide.objects.author = slide.make.group(slide.stage, 0, 0.7);
-  [
-    ["h3", "Your Name"],
-    ["h4", "mail@provider.com"],
-    ["h4", "@yourtwittername"]
-  ].forEach(function(author, i){
-    d3p.theme.phd.heading(slide.objects.author, author[0], author[1]).attr("transform", "translate(0," + d3p.y(i * 0.1) + ")");
+  slide.fragments.push(function(slide){
+    slide.objects.subtitle = d3p.group(0, 0.1)
+      .append("text")
+      .text("This is pretty cool");
   });
 });
 
 d3p.slides.push(function(slide){
-  slide.objects.heading = d3p.theme.phd.block.heading(slide.stage, "Heading", "Subheading");
+  slide.objects.title = d3p.group(0, 0)
+    .append("text")
+    .text("Content");
 
-  slide.fragments.push(function(){
-    slide.objects.go = slide.make.group(slide.stage, 0, 0);
-    d3p.theme.phd.heading(slide.objects.go, "h3", "Let's go", "center", "middle");
+  slide.fragments.push(function(slide){
+    slide.objects.title.transition()
+      .duration(500)
+      .style("opacity", 0);
   });
 });
 
-d3p.slides.push(function(slide){
-  slide.objects.bg = slide.make.background.image(slide.stage, "assets/background.jpg");
-  slide.objects.heading = d3p.theme.phd.block.heading(slide.stage, "Image Background", "assets/background.jpg");
-
-  slide.animate.async("fadeIn", [slide.objects.bg, slide.objects.heading]);
-});
-
-// Wait for webfonts, then go!
-document.addEventListener('DOMContentLoaded', function(){
-  WebFont.load({
-    custom: { families: ['Open Sans Condensed:n3,n7,i3'] },
-    active: d3p.init
-  });
-});
+// Wait for DOM, and then fire!
+document.addEventListener('DOMContentLoaded', d3p.init);
